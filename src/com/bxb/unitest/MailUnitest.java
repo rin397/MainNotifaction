be47f709a42debb8091df0fe6b0247e5968d1fb8
chengwei <chengwei@10.29.3.34>
2016-03-21 15:03:54 +0800

    mail V1

--- src/com/bxb/unitest/MailUnitest.java
+++ src/com/bxb/unitest/MailUnitest.java
@@ -1,31 +1,49 @@
 package com.bxb.unitest;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.AddressException;
 
 import com.bxb.util.MailNotificationUtil;
 
 public class MailUnitest {
 	
-	public static String mailFrom="";
+	public static String mailTo="chengweichen01@gmail.com";
+
+	public static String pwd="HHZccw199101";
 	
-	public static String mailTo="";
+	public static String mailFrom="tim.chen@bitsxbites.com";
 	
-	public static String mailContent="";
+	public static String mailContent="hello email";
 	
-	public static String mailAttachment="";
+	public static String mailAttachment="./LP_geometry.pdf";
 	
-	public static String mailTitle="";
+	public static String mailTitle="hello";
 	
-	public static List<String> mailTos= new ArrayList<String>();
+	public static String[] mailTos = {"chengweichen01@gmail.com"};
 
 	public static void main(String[] args) {
 		
-		MailNotificationUtil.mailTo(mailFrom, mailTo,mailTitle, mailContent, mailAttachment);
+		try {
+			MailNotificationUtil.mailTo(mailFrom,pwd,mailTo,mailTitle, mailContent, mailAttachment);
+		} catch (AddressException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (MessagingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	
-		MailNotificationUtil.mailToByList(mailFrom,  mailTos,mailTitle, mailContent, mailAttachment);
+//		MailNotificationUtil.mailToByList(mailFrom,pwd, mailTos,mailTitle, mailContent, mailAttachment);ß
 
 	}
+	
+	
 	
 	
 
--- src/com/bxb/util/MailNotificationUtil.java
+++ src/com/bxb/util/MailNotificationUtil.java
@@ -1,19 +1,98 @@
 package com.bxb.util;
 
-import java.util.List;
+import java.util.Date;
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+
+import java.util.Date;
+import java.util.Properties;
+import javax.activation.DataHandler;
+import javax.activation.FileDataSource;
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 
 public class MailNotificationUtil {
 
-	public static void mailToByList(String mailFrom, List<String> mailTos,String mailTitle, String mailContent, String mailAttachment) {
-		System.out.println("its mail to by list process start");
+	public static void mailTo(final String mailFrom,final String pwd, String mailTo,String mailTitle, String mailContent, String mailAttachment) throws AddressException, MessagingException {
+		System.out.println("its mail to by  process start");
+		System.out.println("its mail to by 9999list process end");
+		
+		
+		Properties properties = new Properties();
+		properties.put("mail.smtp.host", "smtp.gmail.com");
+		properties.put("mail.smtp.port", "587");
+		properties.put("mail.smtp.auth", "true");
+		properties.put("mail.smtp.starttls.enable", "true");
 
-		System.out.println("its mail to by list process end");
+		
+		
+		
+//		Properties.put("mail.store.protocol", "pop3s");
+//		Properties.put("mail.pop3.host", "pop.gmail.com");     
+//		Properties.put("mail.pop3.user", Application.email);
+//		Properties.put("mail.pop3.socketFactory", 995);
+//		Properties.put("mail.pop3.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+//		Properties.put("mail.pop3.port", 995);
+
+		// creates a new session with an authenticator
+		Authenticator auth = new Authenticator() {
+			public PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(mailFrom, pwd);
+			}
+		};
+
+		Session session = Session.getInstance(properties, auth);
+
+		// creates a new e-mail message
+		Message msg = new MimeMessage(session);
+
+		msg.setFrom(new InternetAddress(mailFrom));
+		InternetAddress[] toAddresses = { new InternetAddress(mailTo) };
+		msg.setRecipients(Message.RecipientType.TO, toAddresses);
+		msg.setSubject(mailTitle);
+		msg.setSentDate(new Date());
+		msg.setText(mailContent);
+		
+		String messageText = "Sending a file with FileDataSource\n";
+		 MimeBodyPart messageBodyPart1 = new MimeBodyPart();
+         messageBodyPart1.setText(messageText);
+
+         MimeBodyPart messageBodyPart2 = new MimeBodyPart();
+
+         // attach the file to the message
+         FileDataSource fdatasource = new FileDataSource(mailAttachment);
+         messageBodyPart2.setDataHandler(new DataHandler(fdatasource));
+         messageBodyPart2.setFileName(fdatasource.getName());
+         
+         Multipart mpart = new MimeMultipart();
+         mpart.addBodyPart(messageBodyPart1);
+         mpart.addBodyPart(messageBodyPart2);
+         msg.setContent(mpart);
+
+         
+		// sends the e-mail
+		Transport.send(msg);
 	}
 
-	public static void mailTo(String mailFrom, String mailTo,String mailTitle, String mailContent, String mailAttachment) {
-		System.out.println("its mail to process start22");
+	
+	
+	
+	public static void mailToByList(String mailFrom,String pwd,String[] mailTos, String mailTitle, String mailContent, String mailAttachment) {
+		System.out.println("its mail to by 88888list process start");
 		System.out.println("its mail to process end");
 	}
-	
 	
 }
